79194622a58fd2007156bc48979dce228b48506d
hejianhui <314598029@qq.com>
2018-08-15 13:36:07 +0800

    an high performance version

--- parsing_blocks.py
+++ parsing_blocks.py
@@ -1,34 +1,44 @@
 import requests
 import json
-from web3 import utils, exceptions, Web3, HTTPProvider
+from web3 import Web3
 
 URL = 'http://127.0.0.1:21336'
-BEGIN_HEIGHT = 90000
-END_HEIGHT = 97200
+BEGIN_HEIGHT = 97170
+END_HEIGHT = 97190
 
 
-def check_duplicate(current_height, tx_hash):
-    # print('check dup height:', current_height)
-    # print('tx hash:', tx_hash)
-    for height in range(current_height + 1, END_HEIGHT):
-        print('current check height:', height)
-        postdata = json.dumps({'method': 'getblockbyheight', 'params': {'height': height}})
-        response = requests.post(URL, data=postdata, headers={'Content-Type': 'application/json'}).json()
-        result = response['result']
-        for tx in result['tx']:
-            for input in tx['vin']:
-                print('check, current txid:', input['txid'])
-                if input['txid'] == tx_hash:
-                    # print('duplicate transaction hash found, tx hash:', tx_hash)
-                    return True
+def check_duplicate(airdrop_addresses):
+    no_duplicate_addresses = list()
+    print('airdrop address:', airdrop_addresses)
+    for airdrop_address in airdrop_addresses:
+        find_flag = 0
+        print('checking airdrop transaction', airdrop_address['tx_hash'])
+        print('checking airdrop transaction height', airdrop_address['height'])
+        for index, block in enumerate(all_inputs[(airdrop_address['height'] - BEGIN_HEIGHT + 1):]):
+            print('index:', index)
+            for tx in block:
+                for vin in tx:
+                    if vin == airdrop_address['tx_hash']:
+                        print('duplicated hash found:', airdrop_address)
+                        print('duplicated hash found, input is:', vin)
+                        find_flag = 1
 
-    return False
+        if not find_flag:
+            print('no duplicate input after.', airdrop_address)
+            no_duplicate_addresses.append(airdrop_address)
+        else:
+            print('duplicate input find.', airdrop_address)
+    return no_duplicate_addresses
+
+# print('no duplicate:', no_duplicate_addresses)
 
 
-address_set = list()
+addresses_set = list()
 invalid_address_set = list()
+all_inputs = list()
 request_height = BEGIN_HEIGHT
 while True:
+    block_txs = list()
     postdata = json.dumps({'method': 'getblockbyheight', 'params': {'height': request_height}})
     response = requests.post(URL, data=postdata, headers={'Content-Type': 'application/json'}).json()
     result = response['result']
@@ -37,34 +47,43 @@ while True:
 
     for tx in result['tx']:
         if tx['type'] == 2:
+
+            input_txs = [vin['txid'] for vin in tx['vin']]
+            block_txs.append(input_txs)
+
             for attribute in tx['attributes']:
-                if attribute['usage'] != 0x00:
+                if attribute['usage'] == 0x81:
                     data_decoded = bytes.fromhex(attribute['data']).decode()
                     if data_decoded.startswith('AIRDROP'):
                         print(data_decoded)
                         erc20_address = data_decoded.split(':')[-1]
-                        try:
-                            # validate address
-                            utils.validation.validate_address(Web3(HTTPProvider('')).toChecksumAddress(erc20_address))
-                        except exceptions.InvalidAddress as e:
-                            print(e)
+                        # validate address
+                        if not Web3.isAddress(erc20_address):
                             invalid_address_set.append([erc20_address, str(result['height'])])
                             continue
                         print('erc20 address:', erc20_address)
                         print('tx hash:', tx['hash'])
-                        vout_total = 0
-                        for vout in tx['vout']:
-                            vout_total += float(vout['value'])
-                        print(vout_total)
-                        if not check_duplicate(result['height'], tx['hash']):
-                            address_set.append([erc20_address, str(vout_total), str(result['height'])])
+                        vout_total = sum(float(vout['value']) for vout in tx['vout'])
+                        print("total value:", vout_total)
+                        addresses_set.append({
+                            'address:': erc20_address,
+                            'total_value': vout_total,
+                            'height': result['height'],
+                            'tx_hash': tx['hash'],
+                            'input_txids': input_txs
+                        })
+
+    all_inputs.append(block_txs)
     if request_height == END_HEIGHT + 1:
         break
 
-print(address_set)
+print('all inputs:', all_inputs)
+print('address set:', addresses_set)
+no_dup = check_duplicate(addresses_set)
+print(no_dup)
 with open('./addresses.csv', 'w') as f:
-    for item in address_set:
-        f.write(','.join(item) + '\n')
+    for item in no_dup:
+        f.write(str(item) + '\n')
 
 with open('./invalid_addresses.csv', 'w') as f:
     for item in invalid_address_set:
